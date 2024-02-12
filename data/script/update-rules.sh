5b462038f13b794e6042365e426eb373aeb63f52
Sereinfy <vnxscn@gmail.com>
2024-02-12 20:14:15 +0800

    up

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -15,8 +15,8 @@ cd tmp
 
 echo '下载规则'
 rules=(
-#  "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/adblockdns.txt" #dns
-#  "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/adblockfilters.txt" #filters
+  "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/adblockdns.txt" #dns
+  "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/adblockfilters.txt" #filters
   "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/filters.txt"
   
  )
