d6dd13c32bf07a3b6db58115fc5d2dee935135be
Sereinfy <vnxscn@gmail.com>
2024-02-15 13:50:01 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -18,9 +18,6 @@ rules=(
   "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/adblockdns.txt" #dns
   "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/adblockfilters.txt" #filters
   "https://raw.githubusercontent.com/Sereinfy/Adrules/main/rule/filters.txt" #filters
-  "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt" #Cookie
-  "https://www.i-dont-care-about-cookies.eu/abp/" #Cookie
- )
 
 allow=(
   "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/allowlist.txt"
