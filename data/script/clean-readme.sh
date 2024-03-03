0d26759dd310b95dba9aa64828d76f4b8c271175
Sereinfy <vnxscn@gmail.com>
2024-03-03 12:23:51 +0800

    Update clean-readme.sh

--- data/script/clean-readme.sh
+++ data/script/clean-readme.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+mv rules.txt allow.txt dns.txt rule/
+wait
+echo '移动文件到rule目录'
+
 num_rules=`sed -n 's/^! Total count: //p' rule/rules.txt`
 num_allow=`sed -n 's/^! Total count: //p' rule/allow.txt`
 
@@ -21,5 +25,5 @@ sed -i 's/^拦截规则数量.*/拦截规则数量: '$num_rules' /g' README.md
 sed -i 's/^白名单规则数量.*/白名单规则数量: '$num_allow' /g' README.md
 
 sed -i 's/^DNS检测已失效域名.*/DNS检测已失效域名: '$black_count' /g' README.md
-mv rules.txt allow.txt dns.txt rule/
+
 exit
