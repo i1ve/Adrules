197dd4d40b0e2378057d2c38cf7d8d0e9093134c
Sereinfy <vnxscn@gmail.com>
2024-03-03 13:43:26 +0800

    Update title.py

--- data/python/title.py
+++ data/python/title.py
@@ -20,7 +20,7 @@ for file_path in file_list:
 
     # 在文件顶部插入内容
     new_content = f"[Adblock Plus 2.0]\n" \
-                  f"! Title: Adrules\n" \
+                  f"! Title: Sereinfy Adrules\n" \
                   f"! Homepage: https://github.com/Sereinfy/Adrules\n" \
                   f"! Expires: 12 Hours\n" \
                   f"! Version: {beijing_time}（北京时间）\n" \
