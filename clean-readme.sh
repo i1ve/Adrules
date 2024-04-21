63c2a338d1091cbcaaecc150725c3cdf814a52bd
Sereinfy <vnxscn@gmail.com>
2024-04-21 18:53:34 +0800

    up

--- clean-readme.sh
+++ clean-readme.sh
@@ -3,7 +3,7 @@
 echo '移动文件到rule目录'
 
 num_rules=`sed -n 's/^! Blocked domains: //p' rule/adblockdns.txt`
-num_rules=`sed -n 's/^! Blocked domains: //p' rule/adblockfilters.txt`
+num_filters=`sed -n 's/^! Blocked domains: //p' rule/adblockfilters.txt`
 
 # filters/black.txt文件的行数
 black_file="filters/black.txt"
@@ -14,7 +14,8 @@ fi
 
 time=$(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')
 sed -i "s/^更新时间:.*/更新时间: $time  /g" README.md
-sed -i 's/^拦截规则数量.*/拦截规则数量: '$num_rules' /g' README.md
+sed -i 's/^DNS拦截规则数量.*/拦截规则数量: '$num_rules' /g' README.md
+sed -i 's/^DNS拦截规则数量.*/拦截规则数量: '$num_filters' /g' README.md
 
 sed -i 's/^DNS检测已失效域名.*/DNS检测已失效域名: '$black_count' /g' README.md
 
